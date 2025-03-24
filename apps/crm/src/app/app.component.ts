ba6296edffcf6402f55cf17e58abb6e4d48a81cb
2aiconcept <contact@2aiconcept.com>
2025-03-24 16:39:19 +0100

    optimisation bundle et webpack bundle analyzer

--- apps/crm/src/app/app.component.ts
+++ apps/crm/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { UiComponent, VerticalNavbarComponent } from '@monorepo-angular/ui';
 import { NavItems } from '@monorepo-angular/ui';
-
+import * as moment from 'moment';
+import * as dayjs from 'dayjs';
 @Component({
   imports: [UiComponent, VerticalNavbarComponent],
   selector: 'app-root',
@@ -9,6 +10,8 @@ import { NavItems } from '@monorepo-angular/ui';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  public date = moment().format('DD/MM/YYYY');
+  public date2 = dayjs().format('DD/MM/YYYY');
   navItems: NavItems[] = [
     {
       route: 'orders',
