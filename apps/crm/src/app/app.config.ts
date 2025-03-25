4e5f2cb30d6773ca128f5ab507c57377d19637c3
2aiconcept <contact@2aiconcept.com>
2025-03-25 10:46:59 +0100

    mise en place ngrx et creation des fichiers pour auth store

--- apps/crm/src/app/app.config.ts
+++ apps/crm/src/app/app.config.ts
@@ -5,10 +5,20 @@ import {
   withPreloading,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from '../environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes, withPreloading(PreloadAllModules)),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
 };
