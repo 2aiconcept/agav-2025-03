525d406108ebc0c3cc719492df56d0cfed19f768
2aiconcept <contact@2aiconcept.com>
2025-03-25 14:41:13 +0100

    sign up ts

--- apps/crm/src/app/features/login/views/sign-up.component.ts
+++ apps/crm/src/app/features/login/views/sign-up.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../auth-store/auth.actions';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {}
+export class SignUpComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  private store = inject(Store);
+  form!: FormGroup;
+
+  // utiliser la méthode signup() de AuthService
+  signUp() {
+    const user = this.form.value;
+    // console.log(user);
+    this.store.dispatch(AuthActions.signUp(user));
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/'
+          ),
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+    });
+  }
+}
